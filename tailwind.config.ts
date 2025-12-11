c095fb5be165d4f5223fb4e6c31ef08a6238f760
Pankaj <pankaj4u4m@gmail.com>
2025-12-11 16:43:56 +0100

    feat: Revert 3D Hexagon to previous SVG version and clean up related files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
   darkMode: ["class"],
@@ -84,5 +84,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
